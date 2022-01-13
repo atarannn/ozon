02653a81dd24b88f9d04b43048078e82e82961c5
Anastasia Taran <anastasiatarannn@icloud.com>
2022-01-13 17:13:51 +0200

    adaptive layout

--- src/assets/scripts/gulp-modules/developer.js
+++ src/assets/scripts/gulp-modules/developer.js
@@ -8,4 +8,24 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        425: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        575: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        991: {
+            slidesPerView: 5,
+            spaceBetween: 50
+        },
+    }
+
 });
